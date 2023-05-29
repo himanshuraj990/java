917158b5a35a126fe440e2dd3d2a9f32be015fe8
himanshuraj990 <rajhimanshu9900@gmail.com>
2023-05-29 16:08:14 +0530

    new program added.

---
+++ Pattern4.java
@@ -0,0 +1,26 @@
+
+public class Pattern4
+{
+
+	public static void main(String[] args) 
+	{
+		int n=13;
+		
+		for(int i=0; i<n; i++)
+		{
+		 for(int j=0; j<n; j++) 
+		 {
+			 if(i==j) 
+			 {
+				 System.out.print("*");
+			 }
+			 else
+			 {
+				 System.out.print(" ");
+			 }
+		 }
+		 System.out.println();
+		}
+		
+	}
+}
